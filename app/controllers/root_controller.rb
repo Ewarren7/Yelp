da5e461ac6c9a48a296cf7d6757b2ec88750ea6a
github username <github email address>
2014-06-29 16:11:49 -0400

    passing html loc to sinatra

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,12 +1,20 @@
 class RootController < ApplicationController
   
-  get '/' do
+  post '/go' do
     
+    @lat = params[:lat]
+    @lon = params[:lon]
+    binding.pry
     make_bizs (true)
     @all_open = YelpBiz.all_open
     binding.pry
     erb :index  
 
+  end
+
+  get '/' do
+
+    erb :location
   end
 
 
