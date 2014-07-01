4341a0f5b35f48c9b5a1ebab25f82e1887bd85ce
github username <github email address>
2014-07-01 06:30:17 -0400

    Fixed html5 geolocation broken by styling

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -5,7 +5,7 @@ class RootController < ApplicationController
     @lon = params[:lon]
   
     YelpBiz.set_location(@lat, @lon)
-    make_bizs (true)
+    make_bizs ()
     @all_open = YelpBiz.all_open
  
     erb :index  
