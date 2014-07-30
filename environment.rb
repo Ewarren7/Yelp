f7d9ec69a84a831db4c8bee526329afeaef06ed6
github username <github email address>
2014-07-29 20:37:38 -0400

    changed controllers to be added individually instead of by each loop

--- environment.rb
+++ environment.rb
@@ -3,6 +3,9 @@ Bundler.require
 require'pry'
 require 'open-uri'
 
-Dir[File.join(File.dirname(__FILE__), "./app/controllers", "*.rb")].each {|f| require f}
+#Dir[File.join(File.dirname(__FILE__), "./app/controllers", "*.rb")].each {|f| require f}
 
+require './app/controllers/application_controller' 
+require './app/controllers/random_controller' 
+require './app/controllers/root_controller' 
 require_relative 'run'
