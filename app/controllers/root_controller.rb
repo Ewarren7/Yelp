ec9516b759f5689246f6417915ef30047cfb7644
github username <github email address>
2014-07-29 20:39:21 -0400

    fixed heroku internal error bug. re enabled all controllers

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,15 +1,15 @@
 class RootController < ApplicationController
   
-  # post '/go' do
-  #   @lat = params[:lat]
-  #   @lon = params[:lon]
+  post '/go' do
+    @lat = params[:lat]
+    @lon = params[:lon]
   
-  #   YelpBiz.set_location(@lat, @lon)
-  #   make_bizs
-  #   @all_open = YelpBiz.all_open
+    YelpBiz.set_location(@lat, @lon)
+    make_bizs
+    @all_open = YelpBiz.all_open
  
-  #   erb :index  
-  # end
+    erb :index  
+  end
 
   get '/' do
     erb :location
